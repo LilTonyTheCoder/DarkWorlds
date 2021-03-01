ca09925d4bf51e1f11878aa43851a1481e5ffa3c
bigtony555 <ahtohka@gmail.com>
2021-03-01 14:47:14 +0300

    added version

--- components/sections/chat/index.vue
+++ components/sections/chat/index.vue
@@ -3,6 +3,10 @@
     <div>19:22 <b>Чемпион:</b> привет мир</div>
     <div>19:22 <b>bender:</b> а я сгибаю мир</div>
     <div>19:24 <b>bender:</b> кстати, я офф</div>
+
+    <div class="main-chat__release">
+      v 0.001
+    </div>
   </div>
 </template>
 
@@ -28,5 +32,14 @@ export default {
 
   font-size: 14px;
   padding: 0 1%;
+
+  &__release {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    padding: 7px 12px;
+    background: #fff;
+    border: 1px dashed #848484;
+  }
 }
 </style>
